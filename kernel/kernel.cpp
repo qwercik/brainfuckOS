fc12b78b11452a876ce652fab38ca5102ef160ed
Eryk Andrzejewski <erykandrzejewski2000@gmail.com>
2017-10-28 00:22:03 +0200

    Add hello world kernel code

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,14 +1,37 @@
+typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 
-int test = 244;
-char* string = "Heheszki :D";
+static uint16_t* vmem = reinterpret_cast<uint16_t*>(0xB8000);
+
+void putchar(uint8_t character, uint8_t attribute = 0x07)
+{
+	*vmem++ = (attribute << 8) | character; 
+}
+
+int strlen(char* string)
+{
+	int length = 0;
+	while (string[length++]);
+	return --length;
+}
+
+void puts(char* string)
+{
+	for (int i = 0; i < strlen(string); ++i)
+	{
+		putchar(string[i]);
+	}
+}
+
+void halt()
+{
+	for (;;);
+}
+
 
 extern "C"
 void kmain()
 {
-	uint16_t* vmem = (uint16_t*)0xB8000;
-	*vmem = 0x4141;
-	*(vmem + 1) = 0x4242;
-
-	for (;;);
+	puts("Hello world!");
+	halt();
 }
