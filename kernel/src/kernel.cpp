b25902037894eff2d018e446fccc80cd4ba3f918
qwercik <erykandrzejewski@gmail.com>
2020-03-16 22:21:30 +0100

    Print numbers

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <cppsupport.hpp>
 #include <terminal/RawOutput.hpp>
 
@@ -7,9 +8,42 @@ void sleep(int n) {
 	}
 }
 
+void reverseString(char *string, unsigned length) {
+	for (unsigned index = 0; index <= length / 2; ++index) {
+		int temp = string[index];
+		string[index] = string[length - 1 - index];
+		string[length - 1 - index] = temp;
+	}
+}
+
+char* intToString(long int value, char *buffer) {
+	bool negative = value > 0 ? false : true;
+	value = value > 0 ? value : -value;
+
+	unsigned index = 0;
+	while (value > 0) {
+		buffer[index++] = '0' + value % 10;
+		value /= 10;
+	}
+
+	if (negative) {
+		buffer[index++] = '-';
+	}
+
+	buffer[index] = '\0';
+
+	reverseString(buffer, index);
+	return buffer;
+}
+
 extern "C"
 void kmain() {
 	bfos::terminal::RawOutput terminal;
 	terminal.setAttribute(0xF);
-	terminal.printString("Welcome to brainfuckOS \b");
+	terminal.printString("Welcome to brainfuckOS\n");
+	terminal.printString("Moja ulubiona liczba to ");
+
+	char buffer[100];
+	intToString(-112997, buffer);
+	terminal.printString(buffer);
 }
